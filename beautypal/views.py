a4288b244f399b19d4e1ea7e78699539062eb7e1
Lukyhet <lylopezb@outlook.com>
2022-03-25 19:37:08 +0000

    create first models

--- beautypal/views.py
+++ beautypal/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 
-rooms = [
-    {'id': 1, 'name': 'Lets talk skincare!'},
-    {'id': 2, 'name': 'Best haircare chat'},
-    {'id': 3, 'name': 'Makeup and color'},
-]
+#rooms = [
+ #   {'id': 1, 'name': 'Lets talk skincare!'},
+  #  {'id': 2, 'name': 'Best haircare chat'},
+   # {'id': 3, 'name': 'Makeup and color'},
+#]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'beautypal/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'beautypal/room.html', context)
     
