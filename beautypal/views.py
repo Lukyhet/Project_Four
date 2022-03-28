46728ce8197aaffe8bf4d2dbd1c35dafa82f37d0
Lukyhet <lylopezb@outlook.com>
2022-03-28 19:31:32 +0000

    create browser and search bar for groups

--- beautypal/views.py
+++ beautypal/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -11,8 +12,15 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') !=None else ''
+
+    rooms = Room.objects.filter(
+       Q(topic__name__icontains=q) |
+       Q(name__icontains=q) |
+       Q(description__icontains=q)
+
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'beautypal/home.html', context)
 
 def room(request, pk):
@@ -22,13 +30,45 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+
     if request.method == 'POST':
-        print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form': form}
     return render(request, 'beautypal/room_form.html', context)
     
     
 def navbar(request):
     return render(request, 'navbar.html')
+    
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form': form}
+    return render(request, 'beautypal/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'beautypal/delete.html', {'obj': room})
+    
+
+    
+
     
     # Create your views here.
