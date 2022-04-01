774426ae9314d6b8d036640e875a381735654252
Lukyhet <lylopezb@outlook.com>
2022-04-01 23:00:47 +0000

    manage to rescue project by cloning after trying to change the user model in the first one, adjust styling in front end to a more femenine palette

--- beautypal/api/views.py
+++ beautypal/api/views.py
@@ -2,9 +2,9 @@ from django.http import JsonResponse
 
 #from rest_framework.decorators import api_view
 #from rest_framework.response import Response
-#from base.models import Room
-#from .serializers import RoomSerializer
-#from base.api import serializers
+#from beautypal.models import Room
+#from beautypal.serializers import RoomSerializer
+#from beautypal.api import serializers
 
 #@api_view(['GET'])
 def getRoutes(request):
